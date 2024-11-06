dccc934c18e8fc5abd4389f82b6e528ee5370d4d
Ranjana Kumari <ranjanakumari2130@gmail.com>
2024-11-06 21:31:55 +0100

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,17 @@
+#Create a list of sandwich ingredients you can think of and get some of the ingredients out of it in a seperate list.
+
+sandwich_ingredients = ["Cucumber","Tomato","Capsicum","Onion","Cheese","Saus","Salt","Blackpepper","Organo","Chillipepper"]
+print("My sandwich ingredients are:- ")
+print(sandwich_ingredients)
+print("length of list is:-")
+print(len(sandwich_ingredients))
+print(sandwich_ingredients[0:4])
+print(sandwich_ingredients[4:6])
+print(sandwich_ingredients[5:])
+print(sandwich_ingredients[:5])
+print(sandwich_ingredients[6:10])
+print(sandwich_ingredients[4])
+print(sandwich_ingredients[-5:-2])
+print(sandwich_ingredients[:-3])
+print(sandwich_ingredients[:7])
+print(sandwich_ingredients[::-1])
